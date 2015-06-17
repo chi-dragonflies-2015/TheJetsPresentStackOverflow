879b416cd6464370d0aad6a8077f62eb076ef413
Henry Firth <henrynf12@gmail.com>
2015-06-17 15:39:17 -0500

    Add seed file which populates questions with comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 require 'faker'
-
 20.times do
-  Question.create!(
+    question =  Question.create!(
     title: Faker::Lorem.sentence,
     content: Faker::Lorem.paragraph,
     asker_id: rand(100),
     best_answer_id: rand(100))
+  5.times do
+    question.comments << Comment.create!(content: Faker::Lorem.sentence)
+  end
 end
