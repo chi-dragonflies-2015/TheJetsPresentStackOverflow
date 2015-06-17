fcb080f812b6de30f6d9bed79607c8daca5d0cfa
Henry Firth <henrynf12@gmail.com>
2015-06-17 18:03:48 -0500

    Add association between comments and uesrs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
+require 'faker'
+
 jack = User.create!(first_name:"Jack", last_name: "McCallum",email: "mcca@aol.com", password: "abc")
+User.create!(first_name:"Henry", last_name: "Firth",email: "h12@aol.com", password: "123")
 
 
 20.times do
@@ -8,7 +11,7 @@ jack = User.create!(first_name:"Jack", last_name: "McCallum",email: "mcca@aol.co
     asker_id: rand(100),
     best_answer_id: rand(100))
   5.times do
-    question.comments << Comment.create!(content: Faker::Lorem.sentence)
+    question.comments << Comment.create!(content: Faker::Lorem.sentence, commenter_id: 2)
   end
   jack.questions << question
 end
