48ceca25f6c518e1ab8514207620bcf171234ae1
Lola Adekunle & Henry Firth <pair+ac-adekunle.h12@devbootcamp.com>
2015-06-18 09:40:46 -0500

    Cleaned up jquery

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
-    $('html').on('click', '.upvote',function(event){
+    $('html').on('click','.upvote',function(event){
       event.preventDefault();
       var url = $(this).attr('href');
       var request = $.ajax({
@@ -14,10 +8,10 @@ $(document).ready(function() {
         });
       request.done(function(response){
               $('#' + response.id + ' .vote_tally').text(response.tally);
-  console.log(response.tally);
       });
-  });
-    $('html').on('click', '.downvote',function(event){
+    });
+
+    $('html').on('click','.downvote',function(event){
       event.preventDefault();
       var url = $(this).attr('href');
       var request = $.ajax({
