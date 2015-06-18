34b6fb1e3a188beefbaa81060b422524dfb25fbf
Henry Firth <henrynf12@gmail.com>
2015-06-18 13:44:54 -0500

    Add comment functionality to answers

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -15,6 +15,21 @@ post '/questions/:id/comments/new' do
   end
 end
 
+post '/answers/:id/comments/new' do
+  if answer = Answer.find(params[:id])
+    comment = Comment.new(content: params[:content], commenter_id: session[:id].to_i)
+    answer.comments << comment
+  end
+  if request.xhr?
+    id = comment.id
+    content = comment.content
+    content_type :json
+    JSON.generate(id: id, content: content)
+  else
+    redirect "/questions/#{question.id}"
+  end
+end
+
 put '/comments/:id/edit' do
   Comment.find(params[:id]).update_attributes(
   content: params[:content])
